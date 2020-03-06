b18d8afd77b62953fe32298d8f4d8dac355dc7a7
nikolasalim <nikolasalim@gmail.com>
2020-03-06 15:38:10 +0100

    add new message feature

--- index.js
+++ index.js
@@ -1,7 +1,48 @@
 const express = require("express");
 const app = express();
+const Sse = require("json-sse");
+const cors = require("cors");
 
 const port = 4000;
+
+// setting up a fake db
+const db = {};
+db.messages = [];
+
+const corsMiddleware = cors();
+app.use(corsMiddleware);
+
+const parser = express.json();
+app.use(parser);
+
+const stream = new Sse();
+
+app.get("/stream", (req, res) => {
+  const action = {
+    type: "ALL_MESSAGES",
+    payload: db.messages
+  };
+
+  // making new users download previous/old/past data that was already sent
+  // stream.updateInit(db.messages);
+  stream.updateInit(action);
+  stream.init(req, res);
+});
+
+app.post("/message", (req, res) => {
+  const { text } = req.body;
+  db.messages.push(text);
+
+  res.send(text);
+
+  const action = {
+    type: "NEW_MESSAGE",
+    payload: text
+  };
+
+  // sending text to the stream as well
+  stream.send(action);
+});
 
 app.listen(port, () => {
   console.log(`listening on port ${port}!`);
